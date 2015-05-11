fb84497e9bbe931d42ddc02c1b4e8ec704a167d7
Andrew Yale <a.yale9@gmail.com>
2015-05-11 07:36:27 -0400

    make install arguments flags. add program list

--- install.sh
+++ install.sh
@@ -34,82 +34,114 @@ ask() {
     done
 }
 
+if [ $# -eq 0 ]; then
+	echo "Usage: 
+    -p install programs in programs file
+    -d install dropbox
+    -s install solarized
+    -f overwrite dotfiles
+    -t install sublime text 3
+    -h this help message"
+	exit 1 
+fi
 
 user=$(whoami)
 
 if [ $user = "root" ]; then
-	echo
-	else
+	:
+else
 	echo "Not root. Need to use sudo."
 	exit
 fi
 
 
-
-# programs
-if ask "Install Programs?" N; then
-    apt-get install --ignore-missing screen htop autojump ipython python-pip python-dev build-essential emacs24 ack-grep zsh 
-	# oh my zsh
-	curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-	# theme file
-	mv yknot.zsh-theme ~/.oh-my-zsh/themes/
-else
-	echo "Skipping..."
-fi
-
-
-
-
-
-# dropbox
-if ask "Install Dropbox?" N; then
-	curl -L -O https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.02.12_amd64.deb
-	dpkg -i dropbox_2015.02.12_amd64.deb
+while getopts ":pdsfth" opt; do
+	case $opt in
+		p)
+			# programs
+			if  ask "Install Programs?" N; then
+				while read p; do
+					echo "Installing $p"
+					apt-get install $p
+				done <programs
+				# oh my zsh
+				curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+				# theme file
+				mv yknot.zsh-theme ~/.oh-my-zsh/themes/
+			else
+				echo "Skipping..."
+			fi
+			;;
+		d)
+			# dropbox
+			if ask "Install Dropbox?" N; then
+				curl -L -O https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.02.12_amd64.deb
+				dpkg -i dropbox_2015.02.12_amd64.deb
+				
+				rm dropbox_2015.02.12_amd64.deb
+			else
+				echo "Skipping..."
+			fi
+			;;
+		s)
+			if ask "Install Solarized?" N; then
 	
-	rm dropbox_2015.02.12_amd64.deb
-else
-	echo "Skipping..."
-fi
-
-
-
-
-if ask "Install Solarized?" N; then
+				# solarized
+				git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+				cd gnome-terminal-colors-solarized
+				./set_dark.sh
+				
+				
+				cd ..
+				
+				rm -rf gnome-terminal-colors-solarized
+			else
+				echo "Skipping..."
+			fi
+			;;
+		f)
+			if ask "Overwrite dotfiles?" N; then
 	
-	# solarized
-	git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
-	cd gnome-terminal-colors-solarized
-	./set_dark.sh
-
-
-	cd ..
-
-	rm -rf gnome-terminal-colors-solarized
-else
-	echo "Skipping..."
-fi
-
-if ask "Overwrite dotfiles?" N; then
+				# dot files
+				mv dotFiles/* ~/
 	
-	# dot files
-	mv dotFiles/* ~/
-	
-	eval `dircolors ~/.dircolors`
-else
-	echo "Skipping..."
-fi
+				eval `dircolors ~/.dircolors`
+			else
+				echo "Skipping..."
+			fi
+			;;
+		t)
+			# sublime text 3
+			if ask "Install Sublime Text 3?" N; then
+				curl -L -O http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3065_amd64.deb
+				dpkg -i sublime-text_build-3065_amd64.deb
+				
+				rm sublime-text_build-3065_amd64.deb
+			else
+				echo "Skipping..."
+			fi
+			;;
+		h)
+			echo "Usage: 
+    -p install programs in programs file
+    -d install dropbox
+    -s install solarized
+    -f overwrite dotfiles
+    -t install sublime text 3
+    -h this help message" >&2
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			;;
+	esac
+done
+		
 
 
 
-# sublime text 3
-if ask "Install Sublime Text 3?" N; then
-	curl -L -O http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3065_amd64.deb
-	dpkg -i sublime-text_build-3065_amd64.deb
-	
-	rm sublime-text_build-3065_amd64.deb
-else
-	echo "Skipping..."
-fi
+
+
+
 
 
 
